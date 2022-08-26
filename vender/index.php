a79c60372770c6ad839dbe5cd411b9fb8578ffdf
AayushTimalsina-AT <ayushtimalsina19@gmail.com>
2022-08-26 19:47:54 +0545

    code update

--- vender/index.php
+++ vender/index.php
@@ -19,28 +19,33 @@ if(isset($_SESSION['islogin']) && ($_SESSION['islogin'] == true)){ //if login in
     ?>
 <div class= "main">
 <div class= "wrapper">
-    <h1>Dashboard</h1>
-    <div class="col4 text-center">
-     <h1>5</h1>
-    </br>
-     veg
-    </div>
-    <div class="col4 text-center">
-        <h1>5</h1>
-       </br>
-        Chicken
-       </div>
-       <div class="col4 text-center">
-        <h1>5</h1>
-       </br>
-        Buff
-       </div>
-       <div class="col4 text-center">
-        <h1>5</h1>
-       </br>
-        Pork
-       </div>
-       <div class="clearfix"></div>
+   
+    
+        <h1>Order list</h1>
+     <table>
+        <tr>
+            <th>S.N</th>
+            <th>User Name</th>
+            <th>User contact number</th>
+            <th>User Location</th>
+            <th>Food Name</th>
+            <th>Food Image</th>
+            <th>Food Price</th>
+            <th>Resturent Name</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Ram sharma</td>
+            <td>9865329467</td>
+            <td>Imadol, Lalitpur</td>
+            <td>MoMo</td>
+            <td>momo.png</td>
+            <td>Rs.110</td>
+            <td>Xyz.pvt.ltd.</td>
+        </tr>
+     </table>
+   
+    
 </div>
 </div>
 
