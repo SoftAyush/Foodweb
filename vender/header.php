f2867e7d44cce143a75a30c796f9aaf73bf2e44c
AayushTimalsina-AT <ayushtimalsina19@gmail.com>
2022-08-27 23:09:54 +0545

    code change

--- vender/header.php
+++ vender/header.php
@@ -3,7 +3,7 @@
 <head>
 
     <title>FoodWeb Vendor </title>
-    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/3.jpg" type="image/x-icon" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="admin.css">
     <script src="https://kit.fontawesome.com/8440c6416e.js" crossorigin="anonymous"></script>
